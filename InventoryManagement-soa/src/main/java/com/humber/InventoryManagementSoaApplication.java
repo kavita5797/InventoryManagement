27c6133a7ef9be40c7b12ef50b12917ac275d6b9
Kavita_Patel_ <kpatel@imaginovation.net>
2022-07-07 14:19:59 -0400

    base structure

---
+++ InventoryManagement-soa/src/main/java/com/humber/InventoryManagementSoaApplication.java
@@ -0,0 +1,13 @@
+package com.humber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementSoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementSoaApplication.class, args);
+	}
+
+}
---
+++ InventoryManagement-soa/src/test/java/com/humber/InventoryManagementSoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
